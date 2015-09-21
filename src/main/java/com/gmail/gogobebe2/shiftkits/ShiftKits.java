69efbbde23e72e7f1b017f2c6072694cbdc5fe52
gogobebe2 <gogobebe2@gmail.com>
2015-09-21 16:23:57 +1200

    1.0.2-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftkits/ShiftKits.java
+++ src/main/java/com/gmail/gogobebe2/shiftkits/ShiftKits.java
@@ -3,6 +3,7 @@ package com.gmail.gogobebe2.shiftkits;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShiftKits extends JavaPlugin {
+
     @Override
     public void onEnable() {
         getLogger().info("Starting up " + this.getName() + ". If you need me to update this plugin, email at gogobebe2@gmail.com");
