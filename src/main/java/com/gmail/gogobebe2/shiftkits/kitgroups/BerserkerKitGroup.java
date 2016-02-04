179708586c6d976e7fb64c59ec0a9d7a1414d938
gogobebe2 <gogobebe2@gmail.com>
2016-02-04 18:19:13 +1300

    1.1.11-PREALPHA-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftkits/kitgroups/BerserkerKitGroup.java
+++ src/main/java/com/gmail/gogobebe2/shiftkits/kitgroups/BerserkerKitGroup.java
@@ -127,7 +127,7 @@ public class BerserkerKitGroup implements KitGroup {
             }
 
             private short secondsLeftOnCooldown(Player player) {
-                if (isOnCooldown(player)) return (short) ((System.currentTimeMillis() - playersOnCooldown.get(player.getUniqueId())) * 1000);
+                if (isOnCooldown(player)) return (short) ((System.currentTimeMillis() - playersOnCooldown.get(player.getUniqueId())) / 1000);
                 else return 0;
             }
 
